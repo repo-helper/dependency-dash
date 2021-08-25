24b6aedafdd4bcf46c61d4e07d6f5211a7f087da
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-08-25 22:51:46 +0100

    Use sass to build custom CSS

---
+++ rebuild_css.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Compile SCSS for Font Awesome
+scss dependency_dash/static/fontawesome.scss dependency_dash/static/css/fontawesome.min.css --style compressed
+
+# Minify other CSS
+scss dependency_dash/static/css/main.css dependency_dash/static/css/main.min.css --style compressed
+scss dependency_dash/static/css/pygments.css dependency_dash/static/css/pygments.min.css --style compressed
