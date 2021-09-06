f7b0920b24c7a75465d78f0e2ccfae0c0b56ea59
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-09-06 10:14:56 +0100

    Add pre-commit hook for compiling css

--- rebuild_css.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Compile SCSS for Font Awesome
-scss scss/fontawesome.scss dependency_dash/static/css/fontawesome.min.css --style compressed
-
-# Minify other CSS
-scss dependency_dash/static/css/main.css dependency_dash/static/css/main.min.css --style compressed
-scss dependency_dash/static/css/pygments.css dependency_dash/static/css/pygments.min.css --style compressed
---
+++ scripts/rebuild_css.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+RETV=0
+
+compile () {
+  OUTFILE=$2
+  OUTFILE_MAP="$OUTFILE.map"
+
+  if test -f "$OUTFILE"; then
+    CURRENT_SHA=$(sha256sum "$OUTFILE")
+  else
+    CURRENT_SHA=''
+  fi
+
+  if test -f "$OUTFILE_MAP"; then
+    CURRENT_MAP_SHA=$(sha256sum "$OUTFILE_MAP")
+  else
+    CURRENT_MAP_SHA=''
+  fi
+
+  npx sass "$1" "$OUTFILE" --style compressed
+
+  for file in $OUTFILE $OUTFILE_MAP; do
+    sed -i '1s/^\xEF\xBB\xBF//' "$file"
+    sed -i -e '$a\' "$file"
+  done
+
+  if [ "$CURRENT_SHA" != "$(sha256sum "$OUTFILE")" ]; then
+    echo "$OUTFILE changed"
+    git stage "$OUTFILE"
+    RETV=1
+  fi
+
+  if [ "$CURRENT_MAP_SHA" != "$(sha256sum "$OUTFILE_MAP")" ]; then
+    echo "$OUTFILE_MAP changed"
+    git stage "$OUTFILE_MAP"
+    RETV=1
+  fi
+
+}
+
+# Compile SCSS for Font Awesome
+compile "scss/fontawesome.scss" "dependency_dash/static/css/fontawesome.min.css"
+
+# Minify other files
+compile "dependency_dash/static/css/main.css" "dependency_dash/static/css/main.min.css"
+compile "dependency_dash/static/css/pygments.css" "dependency_dash/static/css/pygments.min.css"
+
+exit $RETV
