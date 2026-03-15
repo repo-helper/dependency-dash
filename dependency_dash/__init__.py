5fa87ce11385aaa163bdeb02bf9f3494a9fb3477
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2026-03-15 21:01:33 +0000

    Bump version v0.1.0b1 -> v0.1.0b2

--- dependency_dash/__init__.py
+++ dependency_dash/__init__.py
@@ -38,7 +38,7 @@ from dependency_dash._app import api, app  # noqa: E402
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2021 Dominic Davis-Foster"
 __license__: str = "MIT License"
-__version__: str = "0.1.0b1"
+__version__: str = "0.1.0b2"
 __email__: str = "dominic@davis-foster.co.uk"
 
 __all__ = ["app"]
